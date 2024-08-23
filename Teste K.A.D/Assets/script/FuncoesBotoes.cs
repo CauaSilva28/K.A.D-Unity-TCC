da5c3f7dd892ccdf8cb5f4f34c5f0d17f867044d
Cauã Silva <127631052+CauaSilva28@users.noreply.github.com>
2024-08-23 12:14:10 -0300

    Adicionando telas no menu

--- Teste K.A.D/Assets/script/FuncoesBotoes.cs
+++ Teste K.A.D/Assets/script/FuncoesBotoes.cs
@@ -8,6 +8,10 @@ public class FuncoesBotoes : MonoBehaviour
     public AudioSource somSelectMenu;
     public GameObject telaTransicao;
 
+    public GameObject telaControles;
+    public GameObject telaMenu;
+    public GameObject telaFases;
+
     public void ReiniciarFase(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
@@ -15,6 +19,32 @@ public class FuncoesBotoes : MonoBehaviour
     public void IniciarJogo(){
         somSelectMenu.Play();
         StartCoroutine(inicioJogo());
+    }
+
+    public void AbrirTelaControles()
+    {
+        somSelectMenu.Play();
+        telaControles.SetActive(true);
+        telaMenu.SetActive(false);
+    }
+
+    public void AbrirTelaFases()
+    {
+        somSelectMenu.Play();
+        telaFases.SetActive(true);
+        telaMenu.SetActive(false);
+    }
+
+    public void botaoVoltar()
+    {
+        telaMenu.SetActive(true);
+        telaControles.SetActive(false);
+        telaFases.SetActive(false);
+    }
+
+    public void SairDoJogo()
+    {
+        Application.Quit();
     }
 
     IEnumerator inicioJogo(){
