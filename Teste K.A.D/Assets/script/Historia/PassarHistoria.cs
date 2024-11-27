4af85a0225d85ef8418e4ff39841e919f601496b
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2024-11-27 12:22:27 -0300

    atualizando tela historia

---
+++ Teste K.A.D/Assets/script/Historia/PassarHistoria.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class PassarHistoria : MonoBehaviour
+{
+    public GameObject textoHistoria;
+    public GameObject telaTransicao;
+
+    public Text frases;
+    public Image imgFase;
+
+    public string[] textosFrases;
+    public Sprite[] spritImgHistoria;
+
+    private bool passou = false;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!passou)
+        {
+            StartCoroutine(Passar());
+            passou = true;
+        } 
+    }
+
+    IEnumerator Passar()
+    {
+        for (int i = 0; i < textosFrases.Length; i++)
+        {
+            frases.text = textosFrases[i];
+            imgFase.sprite = spritImgHistoria[i];
+
+            telaTransicao.SetActive(true);
+            telaTransicao.GetComponent<Animator>().SetInteger("transition", 1);
+
+            yield return new WaitForSeconds(1f);
+
+            textoHistoria.GetComponent<Animator>().SetInteger("transition", 1);
+
+            yield return new WaitForSeconds(2f);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.RightArrow));
+
+            textoHistoria.GetComponent<Animator>().SetInteger("transition", 2);
+            telaTransicao.GetComponent<Animator>().SetInteger("transition", 2);
+
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
